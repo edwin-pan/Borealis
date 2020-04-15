15f24516bd863ec905609de490c926bc26058504
Edwin Pan <edwin.h.pan@gmail.com>
2020-04-15 01:33:52 -0700

    adding loading model capabilities for main

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import torchvision
 import models.CNN as CNN
 import numpy as np
 from torch.utils.data.sampler import SubsetRandomSampler
+import os
 
 if __name__ == '__main__':
+    
+    train_flag = False
+    
     # Parameters
-    train_test_split_ratio = 0.1
+    train_test_split_ratio = 0.5
     number_of_iterations = 10
     train_batch_size = 20
     number_of_epochs = 2 # 6
@@ -34,11 +38,20 @@ if __name__ == '__main__':
     
     train_loader = torch.utils.data.DataLoader(datasets, batch_size=train_batch_size, num_workers=0, sampler=train_sampler)
     test_loader = torch.utils.data.DataLoader(datasets, batch_size=num_samples, num_workers=0, sampler=test_sampler)
+    
 
-       
-    # Train the CNN -- Edwin
-    print("[Status] Train CNN")
-    net = CNN.fit(train_loader, number_of_iterations, number_of_epochs)
+    if train_flag:
+        # Train the CNN -- Edwin
+        print("[Status] Train CNN")
+        net = CNN.fit(train_loader, number_of_iterations, number_of_epochs)
+    else:
+        repo_dir = os.path.abspath(os.getcwd())
+        model_dir = os.path.join(repo_dir, 'checkpoints\\net1.model')
+        print("[Status] Load CNN @ ", model_dir)
+        
+        # Load the CNN -- Edwin 
+        net = torch.load(model_dir)
+        net.eval()
     
     print("[Status] Evaluate CNN")
     # Evaluate on Test Set
